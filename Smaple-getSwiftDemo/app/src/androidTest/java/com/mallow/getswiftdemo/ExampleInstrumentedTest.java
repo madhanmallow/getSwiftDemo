122f411abd87d6b9a2ea9afc34937c2b9a8bd6f3
prakash <prakash@mallow-tech.com>
2017-10-26 18:05:40 +0530

    comment change

--- Smaple-getSwiftDemo/app/src/androidTest/java/com/mallow/getswiftdemo/ExampleInstrumentedTest.java
+++ Smaple-getSwiftDemo/app/src/androidTest/java/com/mallow/getswiftdemo/ExampleInstrumentedTest.java
@@ -18,7 +18,7 @@ import static org.junit.Assert.*;
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
-        // Context of the app under test.
+        // Context of the app under stage test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.mallow.getswiftdemo", appContext.getPackageName());
